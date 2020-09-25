1013cbb44ed0ee4c936736acf41b65ad6044de66
s805 <52090688+s805@users.noreply.github.com>
2020-09-25 12:21:03 +0545

    Create data.dart
    data of category file is called from here

---
+++ data.dart
@@ -0,0 +1,73 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:group_project/models/models.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List<Category> categories = [
+  Category(
+    name: 'Accessories',
+    icon: FontAwesomeIcons.tshirt,
+  ),
+  Category(
+    name: 'AutoMobiles',
+    icon: FontAwesomeIcons.car,
+  ),
+  Category(
+    name: 'Beauty and Health',
+    icon: FontAwesomeIcons.hatCowboy,
+  ),
+  Category(
+    name: 'Business & Industrial',
+    icon: FontAwesomeIcons.cashRegister,
+  ),
+  Category(
+    name: 'Book and Learning',
+    icon: FontAwesomeIcons.book,
+  ),
+  Category(
+    name: 'Computer and Peripherals ',
+    icon: FontAwesomeIcons.laptop,
+  ),
+  Category(
+    name: 'Electronics ',
+    icon: FontAwesomeIcons.tv,
+  ),
+  Category(
+    name: 'Event and Happenings ',
+    icon: FontAwesomeIcons.microphone,
+  ),
+  Category(
+    name: 'Computer and Peripherals ',
+    icon: FontAwesomeIcons.laptop,
+  ),
+  Category(
+    name: 'Mobile and Accessories ',
+    icon: FontAwesomeIcons.mobile,
+  ),
+  Category(
+    name: 'Music Instrument ',
+    icon: FontAwesomeIcons.guitar,
+  ),
+  Category(
+    name: 'Pets and Pet care ',
+    icon: FontAwesomeIcons.fish,
+  ),
+  Category(
+    name: 'Real State',
+    icon: FontAwesomeIcons.home,
+  ),
+  Category(
+    name: 'Sports and Fitness',
+    icon: FontAwesomeIcons.footballBall,
+  ),
+  Category(
+    name: 'Toys and Video Games',
+    icon: FontAwesomeIcons.gamepad,
+  ),
+  Category(
+    name: 'Travel, Tours & Packages',
+    icon: FontAwesomeIcons.plane,
+  ),
+];
+
+
